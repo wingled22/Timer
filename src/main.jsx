7ece7ddd4ede3cac59b62742aae3cd91a4da06dc
wingled22 <pelayowindel@gmail.com>
2024-02-12 16:12:24 +0800

    lecheng commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import App2 from './App2.jsx'
-import SampleFooter from './Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
